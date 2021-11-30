246293419b9fa9aca8a84880fa5d357b41869b2a
juergenbossler <95302450+juergenbossler@users.noreply.github.com>
2021-11-30 17:28:53 +0100

    Update pairing_streamlit.py

--- pairing_streamlit.py
+++ pairing_streamlit.py
@@ -5,8 +5,8 @@
 
 import streamlit as st
 #import numpy as np
-import matplotlib
-matplotlib.use("Agg")
+#import matplotlib
+#matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import random
 import collections
