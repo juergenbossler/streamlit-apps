6fb01311573f02924b9a07ecaecb90ca04db37f7
juergenbossler <95302450+juergenbossler@users.noreply.github.com>
2026-01-05 16:26:27 +0100

    Update pandas_Hlzhofenklima.py

--- pandas_Hlzhofenklima.py
+++ pandas_Hlzhofenklima.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import numpy as np
 import plotly.express as px
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
-import glob
+#import glob
 
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(page_title="Herlazhofen Klima 2007-2025", page_icon='GMD-Digital_icon.png', initial_sidebar_state='collapsed', layout="wide")
@@ -126,4 +126,5 @@ fig.update_xaxes(
 )
 
 st.plotly_chart(fig, use_container_width=True)
+
 
